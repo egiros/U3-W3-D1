e53cb7f086ae3f884c3fbc1bf567b4631d15a0b7
egiros <dj-g@hotmail.it>
2024-01-15 17:21:52 +0100

    up

--- d9-hw-start/src/App.js
+++ d9-hw-start/src/App.js
@@ -1,34 +1,20 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-import MainSearch from "./components/MainSearch";
-import CompanySearchResults from "./components/CompanySearchResults";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Container } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import MainSearch from './components/MainSearch'
+import CompanySearchResults from './components/CompanySearchResults'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Favourites from './components/Favourites'
 
 function App() {
   return (
-    <Container>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MainSearch />} />
-          <Route path="/:company" element={<CompanySearchResults />} />
-        </Routes>
-      </BrowserRouter>
-      {/* <h2>
-        Cart:{" "}
-        {cart.cart ? (
-          <div>
-            <p>Name: {cart.cart.name}</p>
-            <p>Qt: {cart.cart.qt}</p>
-          </div>
-        ) : (
-          "No cart set"
-        )}
-      </h2>
-      <button onClick={() => dispatch(setCart({ name: "Mouse", qt: 1 }))}>Set Cart</button>
-      <button onClick={() => dispatch(clearCart())}>Clear Cart</button> */}
-    </Container>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainSearch />} />
+        <Route path="/:companyName" element={<CompanySearchResults />} />
+        <Route path="/favourites" element={<Favourites />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
